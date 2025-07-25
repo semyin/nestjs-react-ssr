68545be990861dc21a094947564304577831edb9
semyin <semyin@kplocker.com>
2025-07-25 17:44:35 +0800

    alias

--- entry-client.tsx
+++ entry-client.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
-import App from './App';
+import App from '@/App';
 
 ReactDOM.hydrateRoot(
   document.getElementById('root')!,
